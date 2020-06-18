3fc5f9c9da095dcea1718c29c86830c0a577d6dc
phaneendrass <phaneendra.ss@ritesoftware.com>
2020-06-18 22:51:08 +0530

    Initial Project Set up

---
+++ convert-rite-api/src/main/java/com/rite/products/convertrite/ConvertRiteApiApplication.java
@@ -0,0 +1,13 @@
+package com.rite.products.convertrite;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConvertRiteApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConvertRiteApiApplication.class, args);
+	}
+
+}
---
+++ convert-rite-api/src/test/java/com/rite/products/convertrite/ConvertRiteApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rite.products.convertrite;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConvertRiteApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
